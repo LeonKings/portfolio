93068e6b1016baed01426bc21d4fdc44d2960f50
SkripsiJal <163810470+SkripsiJal@users.noreply.github.com>
2025-01-17 16:41:59 +0700

    grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,18 +1,35 @@
 import React from "react";
 import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
+import { gridItems } from "@/data";
 
 const Grid = () => {
   return (
     <section id="about">
-      <BentoGrid>
-        {[{ title: "Title 1", description: "Desc1", id: 1 }].map((item, i) => (
-          <BentoGridItem
-            id={item.id}
-            key={item.id}
-            title={item.title}
-            description={item.description}
-          />
-        ))}
+      <BentoGrid className="w-full py-20">
+        {gridItems.map(
+          ({
+            id,
+            title,
+            description,
+            className,
+            img,
+            imgClassName,
+            titleClassName,
+            spareImg,
+          }) => (
+            <BentoGridItem
+              id={id}
+              key={id}
+              title={title}
+              description={description}
+              className={className}
+              img={img}
+              imgClassName={imgClassName}
+              titleClassName={titleClassName}
+              spareImg={spareImg}
+            />
+          )
+        )}
       </BentoGrid>
     </section>
   );
---
+++ components/RecentsProject.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RecentsProject = () => {
+  return <div>RecentsProject</div>;
+};
+
+export default RecentsProject;
